aa8b4e3054d58795d99ea2ad8d86da869fdbe8fe
Richard Hemmer <richard@stormgrass.com>
2016-04-19 13:34:47 +0200

    rebuilt with wunderground for https and did some cleanup

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+//to make sure geolookup works, I'm forcing visitors to use https
+if (window.location.protocol != "https:")
+     window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
+
 //creating the variables so I can use them for the API call
 var long;
 var lat;
@@ -40,68 +44,92 @@ $('#thedate').html(date);
 if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(function(position) {
       lat = position.coords.latitude;
+      console.log(lat);
       long = position.coords.longitude;
-//making the API Call
-      $.getJSON('http://api.openweathermap.org/data/2.5/weather?lat=' + lat + "&lon=" + long + "&units=metric&appid=0c684b406b9375c9b3475fa9542a0dbb", function(json) {
-        console.log(JSON.stringify(json));
+      console.log(long);
+//making the API Call - commented out one was the old one
+      //$.getJSON('http://api.openweathermap.org/data/2.5/weather?lat=' + lat + "&lon=" + long + "&units=metric&appid=0c684b406b9375c9b3475fa9542a0dbb", function(json) {
+//here we have the new wunderground API
+        $.getJSON('https://api.wunderground.com/api/56c831f60007f96c/conditions/q/' + lat + "," + long + ".json", function(json) {
+
 //putting in location, temperature and condition
-        $('#thelocation').html(json.name);
-        $('#thetemp').html(Math.round(json.main.temp));
+        //console.log(json.weather[0].description);
+        var tempC = json.current_observation.temp_c;
+        var tempF = json.current_observation.temp_f;
+        $('#thelocation').html(json.current_observation.display_location.city);
 
-        $('#condition').html(json.weather[0].description);
+        $('#thetemp').html(Math.round(tempC));
 
-
-
-//getting the right class for my icon
-        //complicating it by
-
-  function iconClass() {
-                    var timeStamp = Math.floor(Date.now() / 1000);
-                    var newClassDay = ('wi-owm-day-' + json.weather[0].id);
-                    var newClassNight = ('wi-owm-night-' + json.weather[0].id);
-                    if ((timeStamp > json.sys.sunrise) && (timeStamp < json.sys.sunset))
-                    return newClassDay;
-
-                    else return newClassNight;
+        $('#condition').html(json.current_observation.weather);
 
 
-  }
-//putting that icon there
-$('#thesymbol').addClass(iconClass());
 
+//this function used to choose the right icon depending on the time. Unfortunately
+//weathericons.io doesn't support different daytimes for icons on wunderground
+
+  // function iconClass() {
+  //                   var timeStamp = Math.floor(Date.now() / 1000);
+  //                   var newClassDay = ('wi-wu-' + json.current_observation.icon);
+  //                   var newClassNight = ('wi-owm-night-' + json.weather[0].id);
+  //                   if ((timeStamp > json.sys.sunrise) && (timeStamp < json.sys.sunset))
+  //                   return newClassDay;
+  //
+  //                   else return newClassNight;
+  //
+  //
+  // }
+//finding the icon
+
+var iconNeeded = json.current_observation.icon;
+var newClassDay = ('wi-wu-' + iconNeeded);
+
+//alternatively, I could have used the icons from wunderground, but they're ugly
+//var weatherIcon = 'https://icons.wxug.com/i/c/j/' + iconNeeded + '.gif';
+// $('#thesymbol').prepend('<img id="theImg" src="' + weatherIcon + '" />');
+$('#thesymbol').addClass(newClassDay);
 
 //the background switcher
-        switch (json.weather[0].main) {
-          case 'Rain':
-            $("body").css("background", "url('http://hemmer.tv/weather/rain.jpg')");
+        switch (iconNeeded) {
+          case 'rain':
+          case 'sleet':
+            $("body").css("background", "url('img/rain.jpg')");
             break;
-          case 'Drizzle':
-            $("body").css("background", "url('http://hemmer.tv/weather/drizzle.jpg')");
+          case 'chancerain':
+            $("body").css("background", "url('img/drizzle.jpg')");
             break;
-          case 'Thunderstorm':
-            $("body").css("background", "url('http://hemmer.tv/weather/thunderstorm.jpg')");
+          case 'chancetstorms':
+          case 'tstorms':
+            $("body").css("background", "url('img/thunderstorm.jpg')");
             break;
-          case 'Clear':
-            $("body").css("background", "url('http://hemmer.tv/weather/clear.jpg')");
+          case 'clear':
+          case 'mostlysunny':
+          case 'partlysunny':
+          case 'sunny':
+            $("body").css("background", "url('img/clear.jpg')");
             break;
-          case 'Snow':
-            $("body").css("background", "url('http://hemmer.tv/weather/snow.jpg')");
+          case 'chancesnow':
+          case 'flurries':
+          case 'chanceflurries':
+            $("body").css("background", "url('img/snow.jpg')");
             break;
           case 'Extreme':
-            $("body").css("background", "url('http://hemmer.tv/weather/extreme.jpg')");
+            $("body").css("background", "url('img/extreme.jpg')");
             break;
-          case 'Clouds':
-            $("body").css("background", "url('http://hemmer.tv/weather/clouds.jpg')");
+          case 'cloudy':
+          case 'hazy':
+          case 'mostlycloudy':
+          case 'partlycloudy':
+            $("body").css("background", "url('img/clouds.jpg')");
             break;
-          case 'Atmosphere':
-            $("body").css("background", "url('http://hemmer.tv/weather/atmosphere.jpg')");
+          case 'unknown':
+            $("body").css("background", "url('img/atmosphere.jpg')");
             break;
-          default: $("body").css("background", "url('http://hemmer.tv/weather/atmosphere.jpg')");
+          default: $("body").css("background", "url('img/atmosphere.jpg')");
 
         }
       //the Celsius button
         $('#Celsius').click(function() {
-          $('#thetemp').html(Math.round(json.main.temp));
+          $('#thetemp').html(Math.round(tempC));
           $('#Celsius').addClass('active');
           $('#Fahrenheit').removeClass('active');
           $('#Fahrenheit').insertAfter('#Celsius');
@@ -109,12 +137,12 @@ $('#thesymbol').addClass(iconClass());
         });
       //the Fahrenheit button
         $('#Fahrenheit').click(function() {
-          var temp = $('#thetemp').html();
-          var fahr = Math.round(temp * 9 / 5 + 32);
+          // var temp = $('#thetemp').html();
+          // var fahr = Math.round(temp * 9 / 5 + 32);
           if ($('#Fahrenheit').hasClass('active')) {
             $('#thetemp').html();
           } else {
-          $('#thetemp').html(fahr);
+          $('#thetemp').html(tempF);
           $('#Fahrenheit').addClass('active');
           $('#Celsius').removeClass('active');
           $('#Celsius').insertAfter('#Fahrenheit');
